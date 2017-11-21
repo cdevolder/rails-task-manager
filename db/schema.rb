7910e547d9013ff169624ca068361b85be90a8e9
Christophe de Volder <christophe@skyviewteam.be>
2017-11-21 13:22:19 +0100

    crate task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121120048) do
+ActiveRecord::Schema.define(version: 20171121121143) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
